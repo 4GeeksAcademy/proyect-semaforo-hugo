2c108125059cb94287092005afcbc81abdcd17a8
Hugo Alexander <165641301+Galexhu@users.noreply.github.com>
2025-10-28 15:46:47 +0000

    ya se ve :)

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 const Home = () => {
 
@@ -6,21 +7,25 @@ const Home = () => {
 
 		
 	return (
+		<div className="container">
 		<div className="semaforo">
 			<div 
 				onClick={() => setSeleccionarColor("rojo")}
-				className={"light rojo"+((seleccionarColor === "rojo") ? "encendido" : "")}>
+				className={"light rojo"+((seleccionarColor === "rojo") ? " encendido" : "")}>
 			</div>
 			<div 
 				onClick={() => setSeleccionarColor("amarillo")}
-				className={"light amarillo"+((seleccionarColor === "amarillo") ? "encendido" : "")}>
+				className={"light amarillo"+((seleccionarColor === "amarillo") ? " encendido" : "")}>
 
 			</div>
 			<div 
 				onClick={() => setSeleccionarColor("verde")}
-				className={"light verde"+((seleccionarColor === "verde") ? "encendido" : "")}>
+				className={"light verde"+((seleccionarColor === "verde") ? " encendido" : "")}>
 			</div>
 		</div>
+			<button className="boton" type="button" class="btn" data-bs-toggle="button">Cambie de color aquí</button>
+		</div>
+		
 	);
 };
 
