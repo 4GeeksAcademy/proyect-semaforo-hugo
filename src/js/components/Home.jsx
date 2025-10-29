37320868049486fce020ebfb0b008a275bc00bf9
Hugo Alexander <165641301+Galexhu@users.noreply.github.com>
2025-10-29 09:58:12 +0000

    finish

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,30 +2,40 @@ import React, { useState } from "react";
 
 
 const Home = () => {
+	const [seleccionarColor, setSeleccionarColor] = useState("rojo");
+	const [mostrarNuevoColor, setmostrarNuevoColor] = useState(false)
+	const añadirColor = () => {
+		setmostrarNuevoColor(colorPrevio=>!colorPrevio);
+	};
 
-		const[seleccionarColor, setSeleccionarColor]=useState("rojo");
-
-		
 	return (
 		<div className="container">
-		<div className="semaforo">
-			<div 
-				onClick={() => setSeleccionarColor("rojo")}
-				className={"light rojo"+((seleccionarColor === "rojo") ? " encendido" : "")}>
+			<div className="semaforo">
+				<div
+					onClick={() => setSeleccionarColor("rojo")}
+					className={"light rojo" + ((seleccionarColor === "rojo") ? " encendido" : "")}>
+				</div>
+				<div
+					onClick={() => setSeleccionarColor("amarillo")}
+					className={"light amarillo" + ((seleccionarColor === "amarillo") ? " encendido" : "")}>
+				</div>
+				<div
+					onClick={() => setSeleccionarColor("verde")}
+					className={"light verde" + ((seleccionarColor === "verde") ? " encendido" : "")}>
+				</div>
+				{mostrarNuevoColor && (
+				<div
+					onClick={() => setSeleccionarColor("nuevo")}
+					className={"light nuevo" + (seleccionarColor === "nuevo" ? " encendido" : "")}>
+				</div>
+				)}
+					
 			</div>
-			<div 
-				onClick={() => setSeleccionarColor("amarillo")}
-				className={"light amarillo"+((seleccionarColor === "amarillo") ? " encendido" : "")}>
+			<button onClick={añadirColor} className="boton d-grid gap-2 col-6 mx-auto">
+			{mostrarNuevoColor ? "Ocultar color" : "Añadir color AQUÍ"}
+			</button>
+			</div>
 
-			</div>
-			<div 
-				onClick={() => setSeleccionarColor("verde")}
-				className={"light verde"+((seleccionarColor === "verde") ? " encendido" : "")}>
-			</div>
-		</div>
-			<button className="boton" type="button" class="btn" data-bs-toggle="button">Cambie de color aquí</button>
-		</div>
-		
 	);
 };
 
